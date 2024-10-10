344e04faf04a5916e6223d0a6c9e5bb4fb6911d8
kishlakhub <gsubbotin.2019@gmail.com>
2024-10-10 17:20:05 +0500

    Разработанный класс отправки сообщения на почту.Subbotin

---
+++ RegIN_Subbotin/Classes/SendMail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Subbotin.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("screamtoxic@yandex.ru", "grihanatoper123"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("screamtoxic@yandex.ru", To, "Проект RegIN_Subbotin", Message);
+        }
+    }
+}
